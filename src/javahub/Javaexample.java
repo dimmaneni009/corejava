bd4adcf6338f0f60435ae4cfc829c858a0fb5833
dimmaneni009 <dimmaneni009@gmail.com>
2024-11-05 20:53:14 -0600

    added

---
+++ src/javahub/Javaexample.java
@@ -0,0 +1,20 @@
+package javahub;
+
+public class Javaexample {
+	public static void main(String[] args){
+		
+	}
+	
+	int a =1; 
+	int b = 2;
+	
+	int c = a + b;
+	
+	int add(){
+		return c;
+	}
+	
+	
+	
+
+}
